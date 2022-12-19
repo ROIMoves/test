b7077b2f3558241111f902f85838f1b7aa20e4c5
ROI <rouziesherica@gmail.com>
2022-12-20 00:40:22 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from web3 import Web3
+from termcolor import cprint
+import time
+import json
+import random
+from tqdm import tqdm
+import decimal
+import requests
+from tabulate import tabulate
+from decimal import Decimal
+
+
+# ROI Moves
+
+print("Hello World")
